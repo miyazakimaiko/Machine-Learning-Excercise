148b4fe0ba1633e1adaaec9b9f8dff7da6f60c85
miyazakimaiko <myzkmik19922@gmail.com>
2020-02-04 22:40:11 +0000

    implemented func to find param with lowest error

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,32 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+C_list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+s_list = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+% create a blank results matrix
+results = zeros(length(C_list) * length(s_list), 3); 
 
+row = 1;
+for i=1:length(C_list)
+    for j=1:length(s_list)
 
+        model = svmTrain(X, y, C_list(i), @(x1, x2) gaussianKernel(x1, x2, s_list(j)));
+        predictions = svmPredict(model, Xval);
+        err_val = mean(double(predictions ~= yval));
+        % save the results in the matrix
+        results(row,:) = [C_list(i) s_list(j) err_val];
+       row = row + 1;
+    endfor
+endfor
 
+% use the min() function on the results matrix to find 
+%   the C and sigma values that give the lowest validation error
 
+lowest_error = find(results(:,3) == min(results(:,3)));
+lowest_set = results(lowest_error, :);
+C = lowest_set(1);
+sigma = lowest_set(2);
 
 % =========================================================================
 

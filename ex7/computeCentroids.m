a86df32da726df92f8f2bed99e05d56869949111
miyazakimaiko <myzkmik19922@gmail.com>
2020-02-09 22:48:57 +0000

    func to compute new location for centroid

--- ex7/computeCentroids.m
+++ ex7/computeCentroids.m
@@ -25,14 +25,10 @@ centroids = zeros(K, n);
 %
 % Note: You can use a for-loop over the centroids to compute this.
 %
-
-
-
-
-
-
-
-
+for i=1:K,
+    selection = find(idx == i);
+    centroids(i, :) = mean(X(selection, :),1); 
+end
 % =============================================================
 
 
